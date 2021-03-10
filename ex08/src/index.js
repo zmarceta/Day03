dc578c05e63333e9273a55798bc8e2564f2d5c1a
zmarceta <zmarceta83@gmail.com>
2021-03-10 18:00:24 +0100

    added ex08

---
+++ ex08/src/index.js
@@ -0,0 +1,15 @@
+function main(numOne, numTwo) {
+	// Only change code below this line
+	
+	var sum;
+    sum = numOne + numTwo;
+	
+	// Only change code above this line
+	return sum;
+}
+
+console.log(main(15,8));
+console.log(main(5,28));
+console.log(main(150,425));
+
+module.exports = main;
