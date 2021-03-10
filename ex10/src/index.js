185ae5521ea0821f5663c7871c7c9f001392921b
zmarceta <zmarceta83@gmail.com>
2021-03-10 18:11:31 +0100

    added ex10

---
+++ ex10/src/index.js
@@ -0,0 +1,15 @@
+function main(numOne, numTwo) {
+	// Only change code below this line
+	
+	var product;
+    product = numOne * numTwo;
+	
+	// Only change code above this line
+	return product;
+}
+
+console.log(main(15,3));
+console.log(main(25,8));
+console.log(main(425,10));
+
+module.exports = main;
