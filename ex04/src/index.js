cadb149b3fd4fffbfbe3c2cd93001ad4ef8351bf
zmarceta <zmarceta83@gmail.com>
2021-03-10 16:56:08 +0100

    added ex04

---
+++ ex04/src/index.js
@@ -0,0 +1,14 @@
+function main() {
+	// Only change code below this line
+	
+	x = 10;
+    y = x;
+	
+	// Only change code above this line
+    return [x, y];
+	
+}
+
+console.log(main());
+
+module.exports = main;
