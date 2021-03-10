06240459365e4d88298fb9977adcbd944037eff7
zmarceta <zmarceta83@gmail.com>
2021-03-10 16:33:24 +0100

    added ex02

---
+++ ex02/src/index.js
@@ -0,0 +1,17 @@
+function main() {
+	// Only change code below this line
+	
+	// Lorem Ipsum 
+
+    /*
+    Lorem 
+    Ipsum
+    */
+	
+	// Only change code above this line
+	
+}
+
+main();
+
+module.exports = main;
