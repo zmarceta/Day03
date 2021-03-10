146151a736417afa4fa014d1daed65f46113a6d1
zmarceta <zmarceta83@gmail.com>
2021-03-10 18:08:12 +0100

    added ex09

---
+++ ex09/src/index.js
@@ -0,0 +1,15 @@
+function main(numOne, numTwo) {
+	// Only change code below this line
+	
+	var sum;
+    sum = numOne - numTwo;
+	
+	// Only change code above this line
+	return sum;
+}
+
+console.log(main(15,8));
+console.log(main(25,8));
+console.log(main(425,120));
+
+module.exports = main;
