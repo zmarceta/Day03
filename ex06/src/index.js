e97b466f62c9dc4a81499b149f388b68e26ec96a
zmarceta <zmarceta83@gmail.com>
2021-03-10 17:18:43 +0100

    added ex06

---
+++ ex06/src/index.js
@@ -0,0 +1,21 @@
+function main() {
+	// Only change code below this line
+	
+	var x;
+    var y;
+    var z;
+    x = 7;
+    y = 12;
+    z = "My name is ";
+	
+	// Only change code above this line
+    x = x + 2;
+    y = y + 4;
+    z = z + " John!";
+    return [x, y, z];
+	
+}
+
+main();console.log(main());
+
+module.exports = main;
